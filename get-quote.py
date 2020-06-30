c413932e305872fdb9daccc8a39b666f52f2d2e9
Dionis Wang <dionis.wang@gmail.com>
2020-06-30 19:19:26 -0400

    added minor features

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,25 @@ def primer():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
   last = len(quotes)-1
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  print("Quote of the day: "+quotes[rnd])
+  cmd = input("Enter any key for another quote 'a' to add quotes or 'q' to exit: ")
+  while(cmd != "q"):
+      if(cmd == "a"):
+          f=open("quotes.txt","w")
+          nquote= input("Enter new quote: ")
+          quotes.append(nquote)
+          total=""
+          for s in quotes:
+              total += s
+          f.write(total)
+          f.close()
+      else:
+        rnd = random.randint(0,last)
+        print(quotes[rnd])
+      cmd = input("Enter any key for another quote 'a' to add quotes or 'q' to exit: ")
+  
 
 if __name__== "__main__":
   primer()
